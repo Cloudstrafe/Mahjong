a34bd769b0b8df5fd5228b084fd23b2250f47ed9
Cloudstrafe <barnh043@umn.edu>
2020-10-02 19:30:17 -0500

    Test coverage for AllTerminalsAndHonorsYaku

--- src/test/java/mahjong/yaku/TestAllTerminalsAndHonorsYaku.java
+++ src/test/java/mahjong/yaku/TestAllTerminalsAndHonorsYaku.java
@@ -10,37 +10,37 @@ import java.util.List;
 
 import static org.junit.Assert.*;
 
-public class TestAllHonorsYaku {
-    AllHonorsYaku allHonorsYaku;
+public class TestAllTerminalsAndHonorsYaku {
+    AllTerminalsAndHonorsYaku allTerminalsAndHonorsYaku;
     Player player;
 
     @Before
     public void setUp() {
-        allHonorsYaku = new AllHonorsYaku();
+        allTerminalsAndHonorsYaku = new AllTerminalsAndHonorsYaku();
         player = new Player(SuitConstants.WEST_WIND, false, 4, "a");
     }
 
     @Test
     public void happyPathGetters() {
         //Expect
-        assertFalse(allHonorsYaku.isMangan());
-        assertEquals(0, allHonorsYaku.getClosedPoints());
-        assertEquals(0, allHonorsYaku.getOpenPoints());
-        assertEquals(0, allHonorsYaku.getCurrentPoints(player));
-        assertFalse(allHonorsYaku.isStackable());
-        assertTrue(allHonorsYaku.isYakuman());
-        assertFalse(allHonorsYaku.isDoubleYakuman());
+        assertFalse(allTerminalsAndHonorsYaku.isMangan());
+        assertEquals(2, allTerminalsAndHonorsYaku.getClosedPoints());
+        assertEquals(2, allTerminalsAndHonorsYaku.getOpenPoints());
+        assertEquals(0, allTerminalsAndHonorsYaku.getCurrentPoints(player));
+        assertFalse(allTerminalsAndHonorsYaku.isStackable());
+        assertFalse(allTerminalsAndHonorsYaku.isYakuman());
+        assertFalse(allTerminalsAndHonorsYaku.isDoubleYakuman());
     }
 
     @Test
     public void happyPathIsValid() {
         //Given
-        Tile t1 = new WindTile(SuitConstants.EAST_WIND);
-        Tile t2 = new WindTile(SuitConstants.EAST_WIND);
-        Tile t3 = new WindTile(SuitConstants.EAST_WIND);
-        Tile t4 = new DragonTile(SuitConstants.GREEN_DRAGON);
-        Tile t5 = new DragonTile(SuitConstants.GREEN_DRAGON);
-        Tile t6 = new DragonTile(SuitConstants.GREEN_DRAGON);
+        Tile t1 = new NumberTile(1, SuitConstants.BAMBOO, false);
+        Tile t2 = new NumberTile(1, SuitConstants.BAMBOO, false);
+        Tile t3 = new NumberTile(1, SuitConstants.BAMBOO, false);
+        Tile t4 = new NumberTile(9, SuitConstants.DOTS, false);
+        Tile t5 = new NumberTile(9, SuitConstants.DOTS, false);
+        Tile t6 = new NumberTile(9, SuitConstants.DOTS, false);
         Tile t7 = new DragonTile(SuitConstants.WHITE_DRAGON);
         Tile t8 = new DragonTile(SuitConstants.WHITE_DRAGON);
         Tile t9 = new WindTile(SuitConstants.WEST_WIND);
@@ -55,20 +55,46 @@ public class TestAllHonorsYaku {
         player.setPlayArea(playArea);
 
         //Expect
-        assertTrue(allHonorsYaku.isValid(player));
+        assertTrue(allTerminalsAndHonorsYaku.isValid(player));
     }
 
     @Test
-    public void handWithNonHonorTilesIsNotValid() {
+    public void handWithSimplesIsNotValid() {
+        //Given
+        Tile t1 = new NumberTile(2, SuitConstants.BAMBOO, false);
+        Tile t2 = new NumberTile(2, SuitConstants.BAMBOO, false);
+        Tile t3 = new NumberTile(2, SuitConstants.BAMBOO, false);
+        Tile t4 = new NumberTile(9, SuitConstants.DOTS, false);
+        Tile t5 = new NumberTile(9, SuitConstants.DOTS, false);
+        Tile t6 = new NumberTile(9, SuitConstants.DOTS, false);
+        Tile t7 = new DragonTile(SuitConstants.WHITE_DRAGON);
+        Tile t8 = new DragonTile(SuitConstants.WHITE_DRAGON);
+        Tile t9 = new WindTile(SuitConstants.WEST_WIND);
+        Tile t10 = new WindTile(SuitConstants.WEST_WIND);
+        Tile t11 = new WindTile(SuitConstants.WEST_WIND);
+        Tile t12 = new DragonTile(SuitConstants.RED_DRAGON);
+        Tile t13 = new DragonTile(SuitConstants.RED_DRAGON);
+        Tile t14 = new DragonTile(SuitConstants.RED_DRAGON);
+        List<Tile> hand = new ArrayList<>(Arrays.asList(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14));
+        PlayArea playArea = new PlayArea();
+        playArea.setHand(hand);
+        player.setPlayArea(playArea);
+
+        //Expect
+        assertFalse(allTerminalsAndHonorsYaku.isValid(player));
+    }
+
+    @Test
+    public void handWithoutFiveDistinctTypesIsNotValid() {
         //Given
         Tile t1 = new NumberTile(1, SuitConstants.BAMBOO, false);
         Tile t2 = new NumberTile(1, SuitConstants.BAMBOO, false);
         Tile t3 = new NumberTile(1, SuitConstants.BAMBOO, false);
-        Tile t4 = new DragonTile(SuitConstants.GREEN_DRAGON);
-        Tile t5 = new DragonTile(SuitConstants.GREEN_DRAGON);
-        Tile t6 = new DragonTile(SuitConstants.GREEN_DRAGON);
+        Tile t4 = new NumberTile(9, SuitConstants.DOTS, false);
+        Tile t5 = new NumberTile(9, SuitConstants.DOTS, false);
+        Tile t6 = new NumberTile(9, SuitConstants.DOTS, false);
         Tile t7 = new DragonTile(SuitConstants.WHITE_DRAGON);
-        Tile t8 = new DragonTile(SuitConstants.WHITE_DRAGON);
+        Tile t8 = new DragonTile(SuitConstants.GREEN_DRAGON);
         Tile t9 = new WindTile(SuitConstants.WEST_WIND);
         Tile t10 = new WindTile(SuitConstants.WEST_WIND);
         Tile t11 = new WindTile(SuitConstants.WEST_WIND);
@@ -81,18 +107,18 @@ public class TestAllHonorsYaku {
         player.setPlayArea(playArea);
 
         //Expect
-        assertFalse(allHonorsYaku.isValid(player));
+        assertFalse(allTerminalsAndHonorsYaku.isValid(player));
     }
 
     @Test
-    public void allHonorsButNotFiveUniqueTileTypesIsNotValid() {
+    public void handWithoutPairIsNotValid() {
         //Given
-        Tile t1 = new DragonTile(SuitConstants.GREEN_DRAGON);
-        Tile t2 = new DragonTile(SuitConstants.GREEN_DRAGON);
-        Tile t3 = new DragonTile(SuitConstants.GREEN_DRAGON);
-        Tile t4 = new DragonTile(SuitConstants.GREEN_DRAGON);
-        Tile t5 = new DragonTile(SuitConstants.WHITE_DRAGON);
-        Tile t6 = new DragonTile(SuitConstants.WHITE_DRAGON);
+        Tile t1 = new NumberTile(1, SuitConstants.BAMBOO, false);
+        Tile t2 = new NumberTile(1, SuitConstants.BAMBOO, false);
+        Tile t3 = new NumberTile(1, SuitConstants.BAMBOO, false);
+        Tile t4 = new NumberTile(9, SuitConstants.DOTS, false);
+        Tile t5 = new NumberTile(9, SuitConstants.DOTS, false);
+        Tile t6 = new NumberTile(9, SuitConstants.DOTS, false);
         Tile t7 = new DragonTile(SuitConstants.WHITE_DRAGON);
         Tile t8 = new WindTile(SuitConstants.WEST_WIND);
         Tile t9 = new WindTile(SuitConstants.WEST_WIND);
@@ -107,32 +133,6 @@ public class TestAllHonorsYaku {
         player.setPlayArea(playArea);
 
         //Expect
-        assertFalse(allHonorsYaku.isValid(player));
-    }
-
-    @Test
-    public void allHonorsButNoPairIsNotValid() {
-        //Given
-        Tile t1 = new WindTile(SuitConstants.NORTH_WIND);
-        Tile t2 = new DragonTile(SuitConstants.GREEN_DRAGON);
-        Tile t3 = new DragonTile(SuitConstants.GREEN_DRAGON);
-        Tile t4 = new DragonTile(SuitConstants.GREEN_DRAGON);
-        Tile t5 = new DragonTile(SuitConstants.WHITE_DRAGON);
-        Tile t6 = new DragonTile(SuitConstants.WHITE_DRAGON);
-        Tile t7 = new DragonTile(SuitConstants.WHITE_DRAGON);
-        Tile t8 = new WindTile(SuitConstants.WEST_WIND);
-        Tile t9 = new WindTile(SuitConstants.WEST_WIND);
-        Tile t10 = new WindTile(SuitConstants.WEST_WIND);
-        Tile t11 = new WindTile(SuitConstants.WEST_WIND);
-        Tile t12 = new DragonTile(SuitConstants.RED_DRAGON);
-        Tile t13 = new DragonTile(SuitConstants.RED_DRAGON);
-        Tile t14 = new DragonTile(SuitConstants.RED_DRAGON);
-        List<Tile> hand = new ArrayList<>(Arrays.asList(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14));
-        PlayArea playArea = new PlayArea();
-        playArea.setHand(hand);
-        player.setPlayArea(playArea);
-
-        //Expect
-        assertFalse(allHonorsYaku.isValid(player));
+        assertFalse(allTerminalsAndHonorsYaku.isValid(player));
     }
 }
