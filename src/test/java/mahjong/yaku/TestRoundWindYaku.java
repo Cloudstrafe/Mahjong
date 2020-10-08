cfdd757dda677bf2aabe2bff3609d137146956a8
Cloudstrafe <barnh043@umn.edu>
2020-10-08 13:18:48 -0500

    Test coverage for RoundWind, SevenPairs, and ThirteenOrphans Yaku

--- src/test/java/mahjong/yaku/TestRoundWindYaku.java
+++ src/test/java/mahjong/yaku/TestRoundWindYaku.java
@@ -10,26 +10,27 @@ import java.util.List;
 
 import static org.junit.Assert.*;
 
-public class TestSeatWindYaku {
-    SeatWindYaku seatWindYaku;
+public class TestRoundWindYaku {
+    RoundWindYaku roundWindYaku;
     Player player;
 
     @Before
     public void setUp() {
-        seatWindYaku = new SeatWindYaku();
+        roundWindYaku = new RoundWindYaku();
         player = new Player(SuitConstants.WEST_WIND, false, 4, "a");
+        RoundWindYaku.setRoundWind(SuitConstants.EAST_WIND);
     }
 
     @Test
     public void happyPathGetters() {
         //Expect
-        assertFalse(seatWindYaku.isMangan());
-        assertEquals(1, seatWindYaku.getClosedPoints());
-        assertEquals(1, seatWindYaku.getOpenPoints());
-        assertEquals(0, seatWindYaku.getCurrentPoints(player));
-        assertTrue(seatWindYaku.isStackable());
-        assertFalse(seatWindYaku.isYakuman());
-        assertFalse(seatWindYaku.isDoubleYakuman());
+        assertFalse(roundWindYaku.isMangan());
+        assertEquals(1, roundWindYaku.getClosedPoints());
+        assertEquals(1, roundWindYaku.getOpenPoints());
+        assertEquals(0, roundWindYaku.getCurrentPoints(player));
+        assertFalse(roundWindYaku.isStackable());
+        assertFalse(roundWindYaku.isYakuman());
+        assertFalse(roundWindYaku.isDoubleYakuman());
     }
 
     @Test
@@ -43,9 +44,9 @@ public class TestSeatWindYaku {
         Tile t6 = new DragonTile(SuitConstants.GREEN_DRAGON);
         Tile t7 = new DragonTile(SuitConstants.WHITE_DRAGON);
         Tile t8 = new DragonTile(SuitConstants.WHITE_DRAGON);
-        Tile t9 = new WindTile(SuitConstants.WEST_WIND);
-        Tile t10 = new WindTile(SuitConstants.WEST_WIND);
-        Tile t11 = new WindTile(SuitConstants.WEST_WIND);
+        Tile t9 = new WindTile(SuitConstants.EAST_WIND);
+        Tile t10 = new WindTile(SuitConstants.EAST_WIND);
+        Tile t11 = new WindTile(SuitConstants.EAST_WIND);
         Tile t12 = new DragonTile(SuitConstants.RED_DRAGON);
         Tile t13 = new DragonTile(SuitConstants.RED_DRAGON);
         Tile t14 = new DragonTile(SuitConstants.RED_DRAGON);
@@ -55,40 +56,11 @@ public class TestSeatWindYaku {
         player.setPlayArea(playArea);
 
         //Expect
-        assertTrue(seatWindYaku.isValid(player));
+        assertTrue(roundWindYaku.isValid(player));
     }
 
     @Test
     public void happyPathSetInMeldsIsValid() {
-        //Given
-        Tile t1 = new NumberTile(1, SuitConstants.BAMBOO, false);
-        Tile t2 = new NumberTile(1, SuitConstants.BAMBOO, false);
-        Tile t3 = new NumberTile(1, SuitConstants.BAMBOO, false);
-        Tile t4 = new DragonTile(SuitConstants.GREEN_DRAGON);
-        Tile t5 = new DragonTile(SuitConstants.GREEN_DRAGON);
-        Tile t6 = new DragonTile(SuitConstants.GREEN_DRAGON);
-        Tile t7 = new DragonTile(SuitConstants.WHITE_DRAGON);
-        Tile t8 = new DragonTile(SuitConstants.WHITE_DRAGON);
-        Tile t9 = new WindTile(SuitConstants.WEST_WIND);
-        Tile t10 = new WindTile(SuitConstants.WEST_WIND);
-        Tile t11 = new WindTile(SuitConstants.WEST_WIND);
-        Tile t12 = new DragonTile(SuitConstants.RED_DRAGON);
-        Tile t13 = new DragonTile(SuitConstants.RED_DRAGON);
-        Tile t14 = new DragonTile(SuitConstants.RED_DRAGON);
-        List<Tile> hand = new ArrayList<>(Arrays.asList(t1, t2, t3, t4, t5, t6, t7, t8, t12, t13, t14));
-        PlayArea playArea = new PlayArea();
-        List<Tile> meldTiles = new ArrayList<>(Arrays.asList(t9, t10, t11));
-        Meld meld = new Meld(meldTiles, true,false);
-        playArea.setHand(hand);
-        playArea.getMelds().add(meld);
-        player.setPlayArea(playArea);
-
-        //Expect
-        assertTrue(seatWindYaku.isValid(player));
-    }
-
-    @Test
-    public void handOrMeldsWithoutSeatWindSetIsNotValid() {
         //Given
         Tile t1 = new NumberTile(1, SuitConstants.BAMBOO, false);
         Tile t2 = new NumberTile(1, SuitConstants.BAMBOO, false);
@@ -113,6 +85,35 @@ public class TestSeatWindYaku {
         player.setPlayArea(playArea);
 
         //Expect
-        assertFalse(seatWindYaku.isValid(player));
+        assertTrue(roundWindYaku.isValid(player));
+    }
+
+    @Test
+    public void handOrMeldsWithoutSeatWindSetIsNotValid() {
+        //Given
+        Tile t1 = new NumberTile(1, SuitConstants.BAMBOO, false);
+        Tile t2 = new NumberTile(1, SuitConstants.BAMBOO, false);
+        Tile t3 = new NumberTile(1, SuitConstants.BAMBOO, false);
+        Tile t4 = new DragonTile(SuitConstants.GREEN_DRAGON);
+        Tile t5 = new DragonTile(SuitConstants.GREEN_DRAGON);
+        Tile t6 = new DragonTile(SuitConstants.GREEN_DRAGON);
+        Tile t7 = new DragonTile(SuitConstants.WHITE_DRAGON);
+        Tile t8 = new DragonTile(SuitConstants.WHITE_DRAGON);
+        Tile t9 = new WindTile(SuitConstants.WEST_WIND);
+        Tile t10 = new WindTile(SuitConstants.WEST_WIND);
+        Tile t11 = new WindTile(SuitConstants.WEST_WIND);
+        Tile t12 = new DragonTile(SuitConstants.RED_DRAGON);
+        Tile t13 = new DragonTile(SuitConstants.RED_DRAGON);
+        Tile t14 = new DragonTile(SuitConstants.RED_DRAGON);
+        List<Tile> hand = new ArrayList<>(Arrays.asList(t1, t2, t3, t4, t5, t6, t7, t8, t12, t13, t14));
+        PlayArea playArea = new PlayArea();
+        List<Tile> meldTiles = new ArrayList<>(Arrays.asList(t9, t10, t11));
+        Meld meld = new Meld(meldTiles, true,false);
+        playArea.setHand(hand);
+        playArea.getMelds().add(meld);
+        player.setPlayArea(playArea);
+
+        //Expect
+        assertFalse(roundWindYaku.isValid(player));
     }
 }
--- src/test/java/mahjong/yaku/TestSevenPairsYaku.java
+++ src/test/java/mahjong/yaku/TestSevenPairsYaku.java
@@ -10,43 +10,43 @@ import java.util.List;
 
 import static org.junit.Assert.*;
 
-public class TestSeatWindYaku {
-    SeatWindYaku seatWindYaku;
+public class TestSevenPairsYaku {
+    SevenPairsYaku sevenPairsYaku;
     Player player;
 
     @Before
     public void setUp() {
-        seatWindYaku = new SeatWindYaku();
+        sevenPairsYaku = new SevenPairsYaku();
         player = new Player(SuitConstants.WEST_WIND, false, 4, "a");
     }
 
     @Test
     public void happyPathGetters() {
         //Expect
-        assertFalse(seatWindYaku.isMangan());
-        assertEquals(1, seatWindYaku.getClosedPoints());
-        assertEquals(1, seatWindYaku.getOpenPoints());
-        assertEquals(0, seatWindYaku.getCurrentPoints(player));
-        assertTrue(seatWindYaku.isStackable());
-        assertFalse(seatWindYaku.isYakuman());
-        assertFalse(seatWindYaku.isDoubleYakuman());
+        assertFalse(sevenPairsYaku.isMangan());
+        assertEquals(2, sevenPairsYaku.getClosedPoints());
+        assertEquals(0, sevenPairsYaku.getOpenPoints());
+        assertEquals(0, sevenPairsYaku.getCurrentPoints(player));
+        assertTrue(sevenPairsYaku.isStackable());
+        assertFalse(sevenPairsYaku.isYakuman());
+        assertFalse(sevenPairsYaku.isDoubleYakuman());
     }
 
     @Test
-    public void happyPathSetInHandIsValid() {
+    public void happyPathFourPointsGetCurrentPoints() {
         //Given
         Tile t1 = new NumberTile(1, SuitConstants.BAMBOO, false);
         Tile t2 = new NumberTile(1, SuitConstants.BAMBOO, false);
-        Tile t3 = new NumberTile(1, SuitConstants.BAMBOO, false);
-        Tile t4 = new DragonTile(SuitConstants.GREEN_DRAGON);
+        Tile t3 = new NumberTile(9, SuitConstants.BAMBOO, false);
+        Tile t4 = new NumberTile(9, SuitConstants.BAMBOO, false);
         Tile t5 = new DragonTile(SuitConstants.GREEN_DRAGON);
         Tile t6 = new DragonTile(SuitConstants.GREEN_DRAGON);
         Tile t7 = new DragonTile(SuitConstants.WHITE_DRAGON);
         Tile t8 = new DragonTile(SuitConstants.WHITE_DRAGON);
-        Tile t9 = new WindTile(SuitConstants.WEST_WIND);
-        Tile t10 = new WindTile(SuitConstants.WEST_WIND);
+        Tile t9 = new WindTile(SuitConstants.EAST_WIND);
+        Tile t10 = new WindTile(SuitConstants.EAST_WIND);
         Tile t11 = new WindTile(SuitConstants.WEST_WIND);
-        Tile t12 = new DragonTile(SuitConstants.RED_DRAGON);
+        Tile t12 = new WindTile(SuitConstants.WEST_WIND);
         Tile t13 = new DragonTile(SuitConstants.RED_DRAGON);
         Tile t14 = new DragonTile(SuitConstants.RED_DRAGON);
         List<Tile> hand = new ArrayList<>(Arrays.asList(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14));
@@ -55,45 +55,42 @@ public class TestSeatWindYaku {
         player.setPlayArea(playArea);
 
         //Expect
-        assertTrue(seatWindYaku.isValid(player));
+        assertEquals(4,sevenPairsYaku.getCurrentPoints(player));
     }
 
     @Test
-    public void happyPathSetInMeldsIsValid() {
+    public void happyPathTwoPointsGetCurrentPoints() {
         //Given
         Tile t1 = new NumberTile(1, SuitConstants.BAMBOO, false);
         Tile t2 = new NumberTile(1, SuitConstants.BAMBOO, false);
-        Tile t3 = new NumberTile(1, SuitConstants.BAMBOO, false);
-        Tile t4 = new DragonTile(SuitConstants.GREEN_DRAGON);
+        Tile t3 = new NumberTile(4, SuitConstants.DOTS, false);
+        Tile t4 = new NumberTile(4, SuitConstants.DOTS, false);
         Tile t5 = new DragonTile(SuitConstants.GREEN_DRAGON);
         Tile t6 = new DragonTile(SuitConstants.GREEN_DRAGON);
         Tile t7 = new DragonTile(SuitConstants.WHITE_DRAGON);
         Tile t8 = new DragonTile(SuitConstants.WHITE_DRAGON);
-        Tile t9 = new WindTile(SuitConstants.WEST_WIND);
-        Tile t10 = new WindTile(SuitConstants.WEST_WIND);
+        Tile t9 = new WindTile(SuitConstants.EAST_WIND);
+        Tile t10 = new WindTile(SuitConstants.EAST_WIND);
         Tile t11 = new WindTile(SuitConstants.WEST_WIND);
-        Tile t12 = new DragonTile(SuitConstants.RED_DRAGON);
+        Tile t12 = new WindTile(SuitConstants.WEST_WIND);
         Tile t13 = new DragonTile(SuitConstants.RED_DRAGON);
         Tile t14 = new DragonTile(SuitConstants.RED_DRAGON);
-        List<Tile> hand = new ArrayList<>(Arrays.asList(t1, t2, t3, t4, t5, t6, t7, t8, t12, t13, t14));
+        List<Tile> hand = new ArrayList<>(Arrays.asList(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14));
         PlayArea playArea = new PlayArea();
-        List<Tile> meldTiles = new ArrayList<>(Arrays.asList(t9, t10, t11));
-        Meld meld = new Meld(meldTiles, true,false);
         playArea.setHand(hand);
-        playArea.getMelds().add(meld);
         player.setPlayArea(playArea);
 
         //Expect
-        assertTrue(seatWindYaku.isValid(player));
+        assertEquals(2,sevenPairsYaku.getCurrentPoints(player));
     }
 
     @Test
-    public void handOrMeldsWithoutSeatWindSetIsNotValid() {
+    public void handWithMeldsIsNotValid() {
         //Given
         Tile t1 = new NumberTile(1, SuitConstants.BAMBOO, false);
         Tile t2 = new NumberTile(1, SuitConstants.BAMBOO, false);
-        Tile t3 = new NumberTile(1, SuitConstants.BAMBOO, false);
-        Tile t4 = new DragonTile(SuitConstants.GREEN_DRAGON);
+        Tile t3 = new NumberTile(4, SuitConstants.DOTS, false);
+        Tile t4 = new NumberTile(4, SuitConstants.DOTS, false);
         Tile t5 = new DragonTile(SuitConstants.GREEN_DRAGON);
         Tile t6 = new DragonTile(SuitConstants.GREEN_DRAGON);
         Tile t7 = new DragonTile(SuitConstants.WHITE_DRAGON);
@@ -101,7 +98,7 @@ public class TestSeatWindYaku {
         Tile t9 = new WindTile(SuitConstants.EAST_WIND);
         Tile t10 = new WindTile(SuitConstants.EAST_WIND);
         Tile t11 = new WindTile(SuitConstants.EAST_WIND);
-        Tile t12 = new DragonTile(SuitConstants.RED_DRAGON);
+        Tile t12 = new WindTile(SuitConstants.WEST_WIND);
         Tile t13 = new DragonTile(SuitConstants.RED_DRAGON);
         Tile t14 = new DragonTile(SuitConstants.RED_DRAGON);
         List<Tile> hand = new ArrayList<>(Arrays.asList(t1, t2, t3, t4, t5, t6, t7, t8, t12, t13, t14));
@@ -113,6 +110,32 @@ public class TestSeatWindYaku {
         player.setPlayArea(playArea);
 
         //Expect
-        assertFalse(seatWindYaku.isValid(player));
+        assertFalse(sevenPairsYaku.isValid(player));
+    }
+
+    @Test
+    public void handWithoutSevenPairsIsNotValid() {
+        //Given
+        Tile t1 = new NumberTile(1, SuitConstants.BAMBOO, false);
+        Tile t2 = new NumberTile(1, SuitConstants.BAMBOO, false);
+        Tile t3 = new NumberTile(4, SuitConstants.DOTS, false);
+        Tile t4 = new NumberTile(4, SuitConstants.DOTS, false);
+        Tile t5 = new DragonTile(SuitConstants.GREEN_DRAGON);
+        Tile t6 = new DragonTile(SuitConstants.GREEN_DRAGON);
+        Tile t7 = new DragonTile(SuitConstants.WHITE_DRAGON);
+        Tile t8 = new DragonTile(SuitConstants.WHITE_DRAGON);
+        Tile t9 = new WindTile(SuitConstants.EAST_WIND);
+        Tile t10 = new WindTile(SuitConstants.EAST_WIND);
+        Tile t11 = new WindTile(SuitConstants.EAST_WIND);
+        Tile t12 = new WindTile(SuitConstants.WEST_WIND);
+        Tile t13 = new DragonTile(SuitConstants.RED_DRAGON);
+        Tile t14 = new DragonTile(SuitConstants.RED_DRAGON);
+        List<Tile> hand = new ArrayList<>(Arrays.asList(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14));
+        PlayArea playArea = new PlayArea();
+        playArea.setHand(hand);
+        player.setPlayArea(playArea);
+
+        //Expect
+        assertFalse(sevenPairsYaku.isValid(player));
     }
 }
