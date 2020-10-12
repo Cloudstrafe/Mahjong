c3bbfb968d619f495e334efbc9a3eaf197269b30
Cloudstrafe <barnh043@umn.edu>
2020-10-12 18:40:07 -0500

    Test coverage for WindTile and NumberTile classes

---
+++ src/test/java/mahjong/tile/TestWindTile.java
@@ -0,0 +1,28 @@
+package mahjong.tile;
+
+import mahjong.SuitConstants;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestWindTile {
+
+    @Test
+    public void happyPathGetters() {
+        //Given
+        Tile tile = new WindTile(SuitConstants.NORTH_WIND);
+
+        //Expect
+        assertFalse(tile.isDragon());
+        assertTrue(tile.isWind());
+        assertFalse(tile.isPrevailingWind());
+        assertFalse(tile.isSeatWind());
+        assertFalse(tile.isNumber());
+        assertFalse(tile.isTerminal());
+        assertTrue(tile.isHonor());
+        assertFalse(tile.isDora);
+        assertFalse(tile.isRed);
+        assertEquals(0, tile.number);
+        assertEquals(SuitConstants.NORTH_WIND, tile.suit);
+    }
+}
