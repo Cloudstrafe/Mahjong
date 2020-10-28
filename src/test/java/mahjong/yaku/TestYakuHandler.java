1c55fe7c16a44dd12d331392ecb70f4ded092921
Cloudstrafe <barnh043@umn.edu>
2020-10-28 16:15:35 -0500

    Added more testing for riichi implementation

--- src/test/java/mahjong/yaku/TestYakuHandler.java
+++ src/test/java/mahjong/yaku/TestYakuHandler.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 
 import java.util.*;
 import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 
@@ -55,7 +56,7 @@ public class TestYakuHandler {
     }
 
     @Test
-    public void testOneDiscardMultipleWaits() {
+    public void testOneDiscardFewWaits() {
         //Given
         Tile t1 = new WindTile(SuitConstants.SOUTH_WIND);
         Tile t2 = new WindTile(SuitConstants.SOUTH_WIND);
@@ -82,15 +83,70 @@ public class TestYakuHandler {
         compareMaps(expected, actual);
     }
 
+    @Test
+    public void testMultipleDiscardsSingleWait() {
+        Tile t1 = new NumberTile(1, SuitConstants.BAMBOO, false);
+        Tile t2 = new NumberTile(1, SuitConstants.BAMBOO, false);
+        Tile t3 = new NumberTile(9, SuitConstants.BAMBOO, false);
+        Tile t4 = new NumberTile(9, SuitConstants.BAMBOO, false);
+        Tile t5 = new DragonTile(SuitConstants.GREEN_DRAGON);
+        Tile t6 = new DragonTile(SuitConstants.GREEN_DRAGON);
+        Tile t7 = new DragonTile(SuitConstants.WHITE_DRAGON);
+        Tile t8 = new DragonTile(SuitConstants.WHITE_DRAGON);
+        Tile t9 = new WindTile(SuitConstants.EAST_WIND);
+        Tile t10 = new WindTile(SuitConstants.EAST_WIND);
+        Tile t11 = new WindTile(SuitConstants.WEST_WIND);
+        Tile t12 = new WindTile(SuitConstants.WEST_WIND);
+        Tile t13 = new DragonTile(SuitConstants.RED_DRAGON);
+        Tile t14 = new WindTile(SuitConstants.SOUTH_WIND);
+        player.getPlayArea().setHand(Arrays.asList(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14));
+        Map<Tile, List<Tile>> expected = new HashMap<>();
+        expected.put(t13, Collections.singletonList(t14));
+        expected.put(t14, Collections.singletonList(t13));
+
+        //When
+        Map<Tile, List<Tile>> actual = YakuHandler.getRiichiTiles(player);
+
+        //Then
+        compareMaps(expected, actual);
+    }
+
+    @Test
+    public void testOneDiscardManyWeights() {
+        //Given
+        Tile t1 = new NumberTile(1, SuitConstants.BAMBOO, false);
+        Tile t2 = new NumberTile(9, SuitConstants.BAMBOO, false);
+        Tile t3 = new NumberTile(1, SuitConstants.CHARACTERS, false);
+        Tile t4 = new NumberTile(9, SuitConstants.CHARACTERS, false);
+        Tile t5 = new NumberTile(1, SuitConstants.DOTS, false);
+        Tile t6 = new NumberTile(9, SuitConstants.DOTS, false);
+        Tile t7 = new DragonTile(SuitConstants.GREEN_DRAGON);
+        Tile t8 = new DragonTile(SuitConstants.RED_DRAGON);
+        Tile t9 = new DragonTile(SuitConstants.WHITE_DRAGON);
+        Tile t10 = new WindTile(SuitConstants.EAST_WIND);
+        Tile t11 = new WindTile(SuitConstants.WEST_WIND);
+        Tile t12 = new WindTile(SuitConstants.NORTH_WIND);
+        Tile t13 = new WindTile(SuitConstants.SOUTH_WIND);
+        Tile t14 = new NumberTile(5, SuitConstants.DOTS, false);
+        player.getPlayArea().setHand(Arrays.asList(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14));
+        Map<Tile, List<Tile>> expected = new HashMap<>();
+        expected.put(t14, Arrays.asList(t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13));
+
+        //When
+        Map<Tile, List<Tile>> actual = YakuHandler.getRiichiTiles(player);
+
+        //Then
+        compareMaps(expected, actual);
+    }
+
     private void compareMaps(Map<Tile, List<Tile>> expected, Map<Tile, List<Tile>> actual) {
         for (Entry<Tile, List<Tile>> entry : expected.entrySet()) {
-            List<Tile> actualValue = actual.get(entry.getKey());
+            List<Tile> actualValue = actual.get(entry.getKey()).stream().sorted().collect(Collectors.toList());
             assertEquals(entry.getValue().size(), actualValue.size());
+            entry.getValue().stream().sorted().collect(Collectors.toList());
             for (int i = 0; i < entry.getValue().size(); i++) {
-               assertEquals(entry.getValue().get(i), actualValue.get(i));
+               assertEquals(entry.getValue().stream().sorted().collect(Collectors.toList()).get(i), actualValue.get(i));
             }
         }
     }
-
-
 }
