f07b48e3b5052c795f0d400ecfa800cb1312aad3
Cloudstrafe <barnh043@umn.edu>
2020-11-19 19:35:46 -0600

    Updated Three Kans Yaku to work properly, finished Three Kans Yaku tests

--- src/main/java/mahjong/yaku/ThreeKansYaku.java
+++ src/main/java/mahjong/yaku/ThreeKansYaku.java
@@ -1,12 +1,9 @@
 package mahjong.yaku;
 
+import mahjong.Meld;
 import mahjong.Player;
-import mahjong.tile.Tile;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-public class ThreeKansYaku extends AbstractYaku{
+public class ThreeKansYaku extends AbstractYaku {
 
     @Override
     public boolean isMangan() {
@@ -30,10 +27,13 @@ public class ThreeKansYaku extends AbstractYaku{
 
     @Override
     public boolean isValid(Player player) {
-        List<Tile> combined = player.getPlayArea().getCombineHandAndMelds();
-        List<Tile> unique = combined.stream().filter(distinctByKey(t -> t.getNumber() + t.getSuit())).distinct().collect(Collectors.toList());
-        List<Long> counts = unique.stream().map(t -> combined.stream().filter(i -> i.getSuit().equals(t.getSuit()) && i.getNumber() == t.getNumber()).count()).sorted().collect(Collectors.toList());
-        return counts.get(counts.size() - 3) == 4 && YakuHandler.hasAPairAndFourSetsOrRuns(player);
+        int count = 0;
+        for (Meld meld : player.getPlayArea().getMelds()) {
+            if (meld.getTiles().size() == 4) {
+                count++;
+            }
+        }
+        return count == 3 && YakuHandler.hasAPairAndFourSetsOrRuns(player);
     }
 
     @Override
--- src/test/java/mahjong/yaku/TestThreeKansYaku.java
+++ src/test/java/mahjong/yaku/TestThreeKansYaku.java
@@ -59,6 +59,44 @@ public class TestThreeKansYaku {
         Tile t16 = new NumberTile(2, SuitConstants.BAMBOO, false);
         Tile t17 = new NumberTile(2, SuitConstants.BAMBOO, false);
 
+        List<Tile> hand = new ArrayList<>(Arrays.asList(t13, t14, t15, t16, t17));
+        List<Tile> tiles1 = new ArrayList<>(Arrays.asList(t5, t6, t7, t8));
+        Meld meld1 = new Meld(tiles1, false, false, -1);
+        List<Tile> tiles2 = new ArrayList<>(Arrays.asList(t9, t10, t11, t12));
+        Meld meld2 = new Meld(tiles2, false, false, -1);
+        List<Tile> tiles3 = new ArrayList<>(Arrays.asList(t1, t2, t3, t4));
+        Meld meld3 = new Meld(tiles3, true, false, 3);
+        List<Meld> melds = new ArrayList<>(Arrays.asList(meld1, meld2, meld3));
+        PlayArea playArea = new PlayArea(1);
+        playArea.setHand(hand);
+        playArea.setMelds(melds);
+        player.setPlayArea(playArea);
+
+        //Expect
+        assertTrue(threeKansYaku.isValid(player));  //Failing due to kan in hand only beeing seen as a triplet
+    }
+
+    @Test
+    public void KanInHandIsNotValid() {
+        //Given
+        Tile t1 = new DragonTile(SuitConstants.GREEN_DRAGON);
+        Tile t2 = new DragonTile(SuitConstants.GREEN_DRAGON);
+        Tile t3 = new DragonTile(SuitConstants.GREEN_DRAGON);
+        Tile t4 = new DragonTile(SuitConstants.GREEN_DRAGON);
+        Tile t5 = new NumberTile(9, SuitConstants.DOTS, false);
+        Tile t6 = new NumberTile(9, SuitConstants.DOTS, false);
+        Tile t7 = new NumberTile(9, SuitConstants.DOTS, false);
+        Tile t8 = new NumberTile(9, SuitConstants.DOTS, false);
+        Tile t9 = new NumberTile(9, SuitConstants.CHARACTERS, false);
+        Tile t10 = new NumberTile(9, SuitConstants.CHARACTERS, false);
+        Tile t11 = new NumberTile(9, SuitConstants.CHARACTERS, false);
+        Tile t12 = new NumberTile(9, SuitConstants.CHARACTERS, false);
+        Tile t13 = new NumberTile(7, SuitConstants.BAMBOO, false);
+        Tile t14 = new NumberTile(8, SuitConstants.BAMBOO, false);
+        Tile t15 = new NumberTile(9, SuitConstants.BAMBOO, false);
+        Tile t16 = new NumberTile(2, SuitConstants.BAMBOO, false);
+        Tile t17 = new NumberTile(2, SuitConstants.BAMBOO, false);
+
         List<Tile> hand = new ArrayList<>(Arrays.asList(t1, t2, t3, t4, t13, t14, t15, t16, t17));
         List<Tile> tiles1 = new ArrayList<>(Arrays.asList(t5, t6, t7, t8));
         Meld meld1 = new Meld(tiles1, false, false, -1);
@@ -71,6 +109,6 @@ public class TestThreeKansYaku {
         player.setPlayArea(playArea);
 
         //Expect
-        assertTrue(threeKansYaku.isValid(player));  //Failing due to kan in hand only beeing seen as a triplet
+        assertFalse(threeKansYaku.isValid(player));  //Failing due to kan in hand only beeing seen as a triplet
     }
 }
