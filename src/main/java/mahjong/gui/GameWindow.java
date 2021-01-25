80fc5829b5e9339036741338712c123e3a87938f
Cloudstrafe <barnh043@umn.edu>
2021-01-25 17:30:54 -0600

    Indicator for when a player is in riichi but has not yet won, in the form of a 1k point stick in front of them

--- src/main/java/mahjong/gui/GameWindow.java
+++ src/main/java/mahjong/gui/GameWindow.java
@@ -1,11 +1,15 @@
 package mahjong.gui;
 
 import mahjong.Player;
+import mahjong.tile.RotateImage;
 import mahjong.tile.Tile;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -16,10 +20,35 @@ public class GameWindow {
     private final JFrame window = new JFrame("Mahjong");
     private GameInfoPanel gameInfoPanel;
     private DoraPanelHolder doraPanelHolder;
+    private BufferedImage playerOneRiichiImage;
+    private BufferedImage playerTwoRiichiImage;
+    private BufferedImage playerThreeRiichiImage;
+    private BufferedImage playerFourRiichiImage;
+    private JLabel playerOneRiichiIndicator;
+    private JLabel playerTwoRiichiIndicator;
+    private JLabel playerThreeRiichiIndicator;
+    private JLabel playerFourRiichiIndicator;
 
     public GameWindow(Player playerOne, Player playerTwo, Player playerThree, Player playerFour) {
         gameInfoPanel = new GameInfoPanel();
         doraPanelHolder = new DoraPanelHolder();
+        setImages();
+        playerOneRiichiIndicator = new JLabel();
+        playerOneRiichiIndicator.setIcon(new ImageIcon(playerOneRiichiImage));
+        playerOneRiichiIndicator.setBounds(800, 850, 321, 32);
+        playerOneRiichiIndicator.setVisible(false);
+        playerTwoRiichiIndicator = new JLabel();
+        playerTwoRiichiIndicator.setIcon(new ImageIcon(playerTwoRiichiImage));
+        playerTwoRiichiIndicator.setBounds(1332, 350, 32, 321);
+        playerTwoRiichiIndicator.setVisible(false);
+        playerThreeRiichiIndicator = new JLabel();
+        playerThreeRiichiIndicator.setIcon(new ImageIcon(playerThreeRiichiImage));
+        playerThreeRiichiIndicator.setBounds(800, 138, 321, 32);
+        playerThreeRiichiIndicator.setVisible(false);
+        playerFourRiichiIndicator = new JLabel();
+        playerFourRiichiIndicator.setIcon(new ImageIcon(playerFourRiichiImage));
+        playerFourRiichiIndicator.setBounds(556, 350, 32, 321);
+        playerFourRiichiIndicator.setVisible(false);
         window.setSize(1920, 1080);
         window.setLocationRelativeTo(null);
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -29,8 +58,26 @@ public class GameWindow {
         addPanels(playerFour);
         window.add(gameInfoPanel.getMainPanel());
         window.add(doraPanelHolder.getMainPanel());
+        window.add(playerOneRiichiIndicator);
+        window.add(playerTwoRiichiIndicator);
+        window.add(playerThreeRiichiIndicator);
+        window.add(playerFourRiichiIndicator);
         window.add(new JPanel());
         window.setVisible(true);
+    }
+
+    protected void setImages() {
+        try {
+            playerOneRiichiImage = ImageIO.read(new File(DoraPanelHolder.RIICHI_STICK_FILEPATH));
+            playerTwoRiichiImage = ImageIO.read(new File(DoraPanelHolder.RIICHI_STICK_FILEPATH));
+            playerThreeRiichiImage = ImageIO.read(new File(DoraPanelHolder.RIICHI_STICK_FILEPATH));
+            playerFourRiichiImage = ImageIO.read(new File(DoraPanelHolder.RIICHI_STICK_FILEPATH));
+        } catch (IOException e) {
+            System.out.println("Failed to load image " + DoraPanelHolder.RIICHI_STICK_FILEPATH);
+        }
+        playerTwoRiichiImage = RotateImage.rotate(playerTwoRiichiImage, 90.0d);
+        playerFourRiichiImage = RotateImage.rotate(playerFourRiichiImage, 90.0d);
+
     }
 
     private void addPanels(Player player) {
@@ -92,14 +139,30 @@ public class GameWindow {
         return doraPanelHolder;
     }
 
+    public JLabel getPlayerOneRiichiIndicator() {
+        return playerOneRiichiIndicator;
+    }
+
+    public JLabel getPlayerTwoRiichiIndicator() {
+        return playerTwoRiichiIndicator;
+    }
+
+    public JLabel getPlayerThreeRiichiIndicator() {
+        return playerThreeRiichiIndicator;
+    }
+
+    public JLabel getPlayerFourRiichiIndicator() {
+        return playerFourRiichiIndicator;
+    }
+
     public static BufferedImage joinBufferedImage(BufferedImage img1, BufferedImage img2) {
 
         //do some calculate first
-        int offset  = 0;
-        int wid = img1.getWidth()+img2.getWidth()+offset;
-        int height = Math.max(img1.getHeight(),img2.getHeight())+offset;
+        int offset = 0;
+        int wid = img1.getWidth() + img2.getWidth() + offset;
+        int height = Math.max(img1.getHeight(), img2.getHeight()) + offset;
         //create a new buffer and draw two image into the new image
-        BufferedImage newImage = new BufferedImage(wid,height, BufferedImage.TYPE_INT_ARGB);
+        BufferedImage newImage = new BufferedImage(wid, height, BufferedImage.TYPE_INT_ARGB);
         Graphics2D g2 = newImage.createGraphics();
         Color oldColor = g2.getColor();
         //fill background
@@ -108,7 +171,7 @@ public class GameWindow {
         //draw image
         g2.setColor(oldColor);
         g2.drawImage(img1, null, 0, 0);
-        g2.drawImage(img2, null, img1.getWidth()+offset, 0);
+        g2.drawImage(img2, null, img1.getWidth() + offset, 0);
         g2.dispose();
         return newImage;
     }
